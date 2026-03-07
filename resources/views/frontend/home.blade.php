6f20f67c1e47b0af1411c4c384415629d60776c1
grafikomsen <grafikomsen@gmail.com>
2026-03-07 08:19:40 +0000

    refonte-SectionAbout commit

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,30 +1,31 @@
 @extends('app.app')
 @section('main')
+
     <!-- HERO SECTION -->
     <section id="Hero">
-        <div class="container flex flex-col items-center md:flex-row gap-10">
+        <div class="container flex flex-col items-center gap-10 md:flex-row">
             <div class="mx-auto md:basis-1/2 lg:basis-2/5 animate-movingY">
                 <img class="w-60 md:w-full" src="{{ asset('frontend-assets/images/home-image.png') }}" alt="hero-image">
             </div>
             <div class="text-center md:basis-1/2 md:text-start lg:basis-3/5">
                 <h1 class="home-title">HAPPY TUMMY WITH TASTY BURGERS</h1>
-                <div class="separator mx-auto md:mx-0"></div>
+                <div class="mx-auto separator md:mx-0"></div>
                 <p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Odio aperiam placeat nesciunt necessitatibus esse, veritatis
                     obcaecati, ipsum odit perferendis laboriosam voluptate dolorem
                     expedita quis repellendus recusandae! Dolorem ducimus quis voluptate?
                 </p>
-                <div class="flex justify-center items-center md:justify-start md:gap-10 gap-4 py-10 text-base">
+                <div class="flex items-center justify-center gap-4 py-10 text-base md:justify-start md:gap-10">
                     <div class="text-center">
-                        <i class="fa-solid text-secondaryColor text-4xl fa-utensils"></i><br>
+                        <i class="text-4xl fa-solid text-secondaryColor fa-utensils"></i><br>
                         Delicious
                     </div>
                     <div class="text-center">
-                        <i class="fa-solid text-secondaryColor text-4xl fa-droplet"></i><br>
+                        <i class="text-4xl fa-solid text-secondaryColor fa-droplet"></i><br>
                         Fresh
                     </div>
                     <div class="text-center">
-                        <i class="fa-brands text-secondaryColor text-4xl fa-envira"></i><br>
+                        <i class="text-4xl fa-brands text-secondaryColor fa-envira"></i><br>
                         Organic
                     </div>
                 </div>
@@ -37,9 +38,9 @@
     <section id="Category">
         <div class="container flex flex-col gap-5 md:flex-row">
             <!-- CARD 1 -->
-            <div class="bg-secondaryColor flex py-3 rounded-lg overflow-hidden">
+            <div class="flex py-3 overflow-hidden rounded-lg bg-secondaryColor">
                 <div class="relative basis-1/3">
-                    <img class="w-34 absolute -bottom-4 -left-4" src="{{ asset('frontend-assets/images/burger-1.png') }}" alt="">
+                    <img class="absolute w-34 -bottom-4 -left-4" src="{{ asset('frontend-assets/images/burger-1.png') }}" alt="">
                 </div>
                 <div class="basis-2/3">
                     <div class="mb-3">
@@ -50,9 +51,9 @@
                 </div>
             </div>
             <!-- CARD 2 -->
-            <div class=" bg-redColor flex py-3 rounded-lg overflow-hidden">
+            <div class="flex py-3 overflow-hidden rounded-lg bg-redColor">
                 <div class="relative basis-1/3">
-                    <img class="w-34 absolute -bottom-4 -left-4" src="{{ asset('frontend-assets/images/snack-1.png') }}" alt="">
+                    <img class="absolute w-34 -bottom-4 -left-4" src="{{ asset('frontend-assets/images/snack-1.png') }}" alt="">
                 </div>
                 <div class="basis-2/3">
                     <div class="mb-3">
@@ -63,9 +64,9 @@
                 </div>
             </div>
             <!-- CARD 3 -->
-            <div class=" bg-greenColor flex py-3 rounded-lg overflow-hidden">
+            <div class="flex py-3 overflow-hidden rounded-lg bg-greenColor">
                 <div class="relative basis-1/3">
-                    <img class="w-34 absolute -bottom-4 -left-4" src="{{ asset('frontend-assets/images/beverage-2.png') }}" alt="">
+                    <img class="absolute w-34 -bottom-4 -left-4" src="{{ asset('frontend-assets/images/beverage-2.png') }}" alt="">
                 </div>
                 <div class="basis-2/3">
                     <div class="mb-3">
@@ -82,28 +83,65 @@
     <section id="Promo">
         <div class="container flex flex-col gap-4 md:flex-row">
             <!-- CARD 1 -->
-            <div class="flex flex-col p-5 bg-primaryColorLight rounded-lg">
+            <div class="flex flex-col p-5 rounded-lg bg-primaryColorLight">
 
                 <img class="w-40 mx-auto hover:animate-movingY" src="{{ asset('frontend-assets/images/promo-1.png') }}" alt="Promo image">
 
-                <div class=" mt-4 space-y-2">
-                    <p class="text-secondaryColor text-sm">Payday promo</p>
+                <div class="mt-4 space-y-2 ">
+                    <p class="text-sm text-secondaryColor">Payday promo</p>
                     <h3 class="card-title">Get 10% discount on payday week</h3>
                     <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     <a class="text-xs text-secondaryColor" href="">Buy online</a>
                 </div>
             </div>
             <!-- CARD 2 -->
-            <div class="flex flex-col p-5 bg-primaryColorLight rounded-lg">
+            <div class="flex flex-col p-5 rounded-lg bg-primaryColorLight">
 
                 <img class="w-40 mx-auto hover:animate-movingY" src="{{ asset('frontend-assets/images/promo-2.png') }}" alt="Promo image">
 
-                <div class=" mt-4 space-y-2">
-                    <p class="text-secondaryColor text-sm">Payday promo</p>
+                <div class="mt-4 space-y-2 ">
+                    <p class="text-sm text-secondaryColor">Payday promo</p>
                     <h3 class="card-title">Get 10% discount on payday week</h3>
                     <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     <a class="text-xs text-secondaryColor" href="">Buy online</a>
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ABOUT SECTION -->
+    <section id="About">
+        <div class="container flex flex-col gap-10 md:flex-row">
+            <div class="flex-1">
+                <img class="rounded-lg" src="{{ asset('frontend-assets/images/about.jpg') }}" alt="">
+            </div>
+            <div class="flex-1">
+                <h2 class="section-title">FIND FOOD AND DRINKS, ALL-IN-ONE PLACE FOR YOUR BEST TASTE.</h2>
+                <div class="separator"></div>
+                <p class="para">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo ligula eget dolor. Aenean massa.
+                    Cum sociis natoque penatibus et magnis dis parturient montes.
+                </p>
+                <ul class="grid grid-cols-2 py-5 space-y-1">
+                    <li class="text-paragraphColor">
+                        <i class="fa-solid fa-check text-secondaryColor"></i>
+                        Best Price
+                    </li>
+                     <li class="text-paragraphColor">
+                        <i class="fa-solid fa-check text-secondaryColor"></i>
+                        Fresh Ingredient
+                    </li>
+                     <li class="text-paragraphColor">
+                        <i class="fa-solid fa-check text-secondaryColor"></i>
+                        Best Service
+                    </li>
+                     <li class=" text-paragraphColor">
+                        <i class="fa-solid fa-check text-secondaryColor"></i>
+                        Health Protocol
+                    </li>
+                </ul>
+                <a class="btn btn-primary" href="">About Us</a>
             </div>
         </div>
     </section>
